145de351c263375e954826a8e83287882c79c393
KishorSuthar <kishornsuthar866@gmail.com>
2025-07-25 22:56:38 +0530

    intial commit

---
+++ 01.Setup_Local_Environment/basic-react-app/src/App.jsx
@@ -0,0 +1,26 @@
+// import Title from "./Title.jsx";
+// import "./App.css";
+// import Description from "./Description.jsx";
+// function App() {
+//   return (
+//     <>
+//       <Title />
+//       <Description />
+//       <Title />
+//       <Description />
+//     </>
+//   )
+// }
+
+import "./App.css";
+import ProductTab from "./ProductTab";
+
+function App() {
+  return (
+    <>
+      <ProductTab />
+    </>
+  );
+}
+
+export default App;
---
+++ 01.Setup_Local_Environment/basic-react-app/src/Product.jsx
@@ -0,0 +1,12 @@
+import "./Product.css";
+
+function Product() {
+  return (
+    <div className="product">
+      <h2>Product Title</h2>
+      <p>Product Description</p>
+    </div>
+  );
+}
+
+export default Product;
